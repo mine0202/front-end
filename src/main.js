7a7ede799647dfb48282109f718c85af3a26adb8
Judy's Dad <mine0202@naver.com>
2022-11-08 09:00:12 +0900

    페이징

--- src/main.js
+++ src/main.js
@@ -2,5 +2,14 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+//  bootstrap  5.x npm import
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap/dist/js/bootstrap.bundle.js'
+// bootstrap-vue import
+import { BootstrapVue } from 'bootstrap-vue'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+
+// bootstrap vue모듈 사용
+Vue.use(BootstrapVue)
 
 createApp(App).use(store).use(router).mount('#app')
